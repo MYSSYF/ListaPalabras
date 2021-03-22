287f40fa17f84d84d65701dde5bbb380dababd95
MYSSYF <jogabolu@gmail.com>
2021-03-22 18:50:13 -0500

    howdy

--- FlavorText/src/js/view/Main.js
+++ FlavorText/src/js/view/Main.js
@@ -1,5 +1,10 @@
 let texts = [];
 let word = [];
+let list = [];
+let pal1;
+let pal2;
+
+
 
 
 function preload() {
@@ -8,37 +13,112 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(600, 800);
+    createCanvas(800, 900);
+ 
+
 
 
 
     for (let index = 0; index < texts.length; index++) {
          arrayTemp = texts[index].split(" ");
+         arrayTemp2 = texts[index].split(" ");
     
-
+      
         for (let index = 0; index < arrayTemp.length; index++) {
-
-            word.push(new nuevo(arrayTemp[index],random(100,500),random(100,500))); 
+            
            
-         
+            
+            word.push(new nuevo(arrayTemp[index],random(50,750),random(-1000,200))); 
+           
+           
         }
+        
+        
+        for (let index = 0; index < arrayTemp2.length; index++) {
+
+            list.push(new nuevo(arrayTemp2[index],100+(index)*50,875,false)); 
+            
+           
+           
+        }
+        
     }
-
-
-    
+  
 }
-
-
-
 
 
 function draw() {
     background(200);
- for (let index = 0; index < word.length; index++) {
-  word[index].draw();
-     
- }
     
 
+ for (let index = 0; index < word.length; index++) {
+
+  word[index].draw();
+  word[index].llenar(0,0,0);
+  word[index].mover();
+    
+ }
+
+ fill(100);
+ rect(0,850,800,50); 
+encont();
+comp();
+
+
+ for (let index = 0; index < 5; index++) {
+ 
+    list[index].draw();
+    list[index].llenar(0,0,0);
+    
+    if(list[index].coger==true){
+        list[index].chas();
+        
+        }
+ } 
+
+
+
+ 
+}
+
+function comp () {
+    
+    if(pal1===pal2){
+console.log("yass");
+for (let index = 0; index < 5; index++) {
+list[index].draw();
+list[index].llenar(0,255,0);
+        }
+    }
+}
+
+
+
+function encont () {
+    pal2 = "pal";
+    for (let index = 0; index < word.length; index++) {
+    if(this.mouseX<=word[index].posx+10&&this.mouseX>=word[index].posx-10&&this.mouseY<=word[index].posy+10&&this.mouseY>=word[index].posy-10){
+pal2 = word[index].palabra
+    }
+}
+}
+
+function mousePressed() {
+    for (let index = 0; index < 5; index++) {
+    if(this.mouseX<=list[index].posx+10&&this.mouseX>=list[index].posx-10&&this.mouseY<=list[index].posy+10&&this.mouseY>=list[index].posy-10){
+      list[index].coger=true;
+      pal1 = list[index].palabra 
+
+
+          }
+        }
+}
+
+function mouseReleased() {
+    for (let index = 0; index < 5; index++) {
+list[index].coger=false;
+pal1 = undefined;
 
 }
+}
+
--- FlavorText/src/js/view/nuevo.js
+++ FlavorText/src/js/view/nuevo.js
@@ -1,18 +1,37 @@
 class nuevo {
 
-    constructor(palabra,posx,posy){
+    constructor(palabra,posx,posy,coger){
 this.posx =posx;
 this.posy =posy;
 this.palabra =palabra;
+this.coger = coger;
+
 
        
     }
 
     draw(){
+    
+        textAlign(CENTER);
+        textSize(18);
        text(this.palabra,this.posx,this.posy);
     }
 
+mover(){
+    this.posy+= random(1,6);
+    if(this.posy>=930){
+this.posy = 10;
+    }
+}
+chas(){
 
+    this.posx = mouseX;
+    this.posy = mouseY;
+}
+
+llenar(r,g,b){
+    fill(r,g,b)
+}
 
           
     }
